fae64caed36153d3bcdc6257846c0c7662b19658
Sebas <sebasweb2000@gmail.com>
2023-03-22 23:27:34 -0400

    fixed import of projects and skills at App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,14 @@
-import { useState } from 'react'
 import AboutMe from './components/AboutMe'
 import Contacts from './components/Contacts'
 import Footer from './components/Footer'
 import Home from './components/Home/Home'
 import Navbar from './components/Home/Navbar'
 import Sidebar from './components/Home/Sidebar'
-import Projects from './components/projects/projects'
-import Skills from './components/Skills/skills'
+import Projects from './components/projects/Projects'
+import Skills from './components/Skills/Skills'
 import './style/hover/hover.css'
 import'./style/icons/icons.css'
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <div className="App">
